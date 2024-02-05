95806f25fd49fcdb5072a5aa8f025e9abde0ff6e
workshops-gormley <159040232+workshops-gormley@users.noreply.github.com>
2024-02-05 10:09:38 -0500

    default entires

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 # Smoothies 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
