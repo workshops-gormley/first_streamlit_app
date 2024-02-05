b492fc71e45284436576229cb8c48aff7b264d67
workshops-gormley <159040232+workshops-gormley@users.noreply.github.com>
2024-02-05 10:12:26 -0500

    selected_show

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,6 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 # Smoothies 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
